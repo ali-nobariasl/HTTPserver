52cca98a34757bdae4ec4bb1abdc9d497c2886f6
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-02-08 12:46:20 +0300

    query params

--- index.py
+++ index.py
@@ -8,13 +8,28 @@ class basicRequestHandler(tornado.web.RequestHandler):
 
 class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
-         self.render('index.html')
+        self.render('index.html')
+
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if(num.isdigit()):
+            result = 'even' if int(num) %2 else 'odd'
+            self.write(f'{num} integer is : {result}')
+        else:
+            self.write(f'{num} is not a number')
+
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+    def get(self,studentname, corseid):
+        self.write(f'welcome {studentname}, the course is {corseid}')
+
 
 if __name__ == '__main__':
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
         (r"/animal", listRequestHandler),
-
+        (r"/iseven", queryParamRequestHandler),
+        (r'/student/([a-z]+)/([0-9]+)', resourceParamRequestHandler), 
     ])
     
     port = 8882
